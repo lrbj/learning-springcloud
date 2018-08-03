21de104b88745e84c5bf85cc6ddd32f49e42156f
H299633 <shuzhen.ye@honeywell.com>
2018-08-03 17:29:37 +0800

    product 拆分中

--- product/product-client/src/main/java/com/imooc/product/client/ProductClient.java
+++ product/product-client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -1,5 +1,11 @@
 package com.imooc.product.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 /**
  * @Author: Kayla,Ye
  * @Description:
@@ -7,4 +13,9 @@ package com.imooc.product.client;
  */
 @FeignClient(name = "product")
 public interface ProductClient {
+    @PostMapping("/product/listForOrder")
+    List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
 }
